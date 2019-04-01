160af1f6464fb362fcca28b5a971bc579c77c487
Cameron <cdigregorio@Camerons-MacBook-Pro.local>
2019-04-01 18:05:01 -0400

    created game logic

--- memory_game/jss/main.js
+++ memory_game/jss/main.js
@@ -1,5 +1,26 @@
 console.log("Up and Running!");
-var cards = ["Queen", "Queen", "King", "King"];
+var cards = [
+	{
+	rank: "Queen",
+	suit: "Hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "Queen",
+	suit: "Diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "King",
+	suit: "Hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "King",
+	suit: "Diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 
 var checkForMatch =function(){
@@ -11,8 +32,10 @@ var checkForMatch =function(){
 }
 
 var flipCard = function(cardId){
-	console.log("User Flipped " + cards[cardId])
-	cardsInPlay.push(cards[cardId]);
+	console.log("User Flipped " + cards[cardId].rank)
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit)
+	cardsInPlay.push(cards[cardId].rank);
 	if (cardsInPlay.length === 2){
 	checkForMatch();
 	}
