b7e12ab1367b858a825060a19aeae64a7b6f5edb
Cameron <cdigregorio@Camerons-MacBook-Pro.local>
2019-04-02 10:54:29 -0400

    created game logic

--- memory_game/jss/main.js
+++ memory_game/jss/main.js
@@ -25,20 +25,31 @@ var cardsInPlay = [];
 
 var checkForMatch =function(){
 	if (cardsInPlay[0] === cardsInPlay[1]){
-		console.log("You Found a Match!");
+		alert("You Found a Match!");
 	}else{
-		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 	}
 }
 
-var flipCard = function(cardId){
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id')
 	console.log("User Flipped " + cards[cardId].rank)
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit)
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage)
 	if (cardsInPlay.length === 2){
 	checkForMatch();
 	}
 }
-flipCard(0);
-flipCard(2);
+
+var createBoard = function(){
+	for(var i=0; i< cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+createBoard()
